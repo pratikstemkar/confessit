028d877e14e4fb012126fd7435c2c1ae7c2ee2e0
pratikstemkar <pratikstemkar@gmail.com>
2024-11-17 11:08:25 +0530

    config fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,15 @@ import PostCard from "./_components/PostCard";
 export default function Home() {
     return (
         <main className="max-w-5xl m-auto mt-10 px-5 lg:px-0">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
-                <PostCard />
-                <PostCard />
-                <PostCard />
-                <PostCard />
-                <PostCard />
-                <PostCard />
+            <div className="relative">
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+                    <PostCard />
+                    <PostCard />
+                    <PostCard />
+                    <PostCard />
+                    <PostCard />
+                    <PostCard />
+                </div>
             </div>
         </main>
     );
